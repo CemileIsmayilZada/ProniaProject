f2f9a27fbccb24285d8eb1a7f3f568e870463f2d
CemileIsmayilZada <5cctf7f@code.edu.az>
2022-12-22 07:13:40 +0400

    upddate web

--- NetExample/WebUI/WebUI/Areas/Admin/Controllers/DashboardController.cs
+++ NetExample/WebUI/WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DashboardController : Controller
     {
+        private AppDbContext _context;
+        public DashboardController(AppDbContext context)
+        {
+            _context=context;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_context.ShippingItems);
         }
     }
 }
---
+++ NetExample/WebUI/WebUI/Areas/Admin/Controllers/ShippingItemController.cs
@@ -0,0 +1,48 @@
+﻿using Core.Entities;
+using DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ShippingItemController : Controller
+    {
+        private AppDbContext _context;
+
+        public ShippingItemController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            return View(_context.ShippingItems);
+        }
+        public async Task<IActionResult> Detail(int id)
+        {
+            var model = await _context.ShippingItems.FindAsync(id);
+            return View(model);
+        }
+        public async Task<IActionResult> Update(int id)
+        {
+            return View(_context.ShippingItems);
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ShippingItem shipping)
+        {
+            if (!ModelState.IsValid) return View(shipping);
+            await _context.ShippingItems.AddAsync(shipping);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(_context.ShippingItems);
+        }
+    }
+}
